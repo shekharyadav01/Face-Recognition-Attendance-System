a59c32aeb9158d897153f94a84e1cddea11ffafc
Shekhar Yadav <114596028+shekharyadav01@users.noreply.github.com>
2024-02-06 20:08:47 +0530

    Sample.py

--- Sample.py
+++ Sample.py
@@ -4,30 +4,30 @@ import cv2
 import numpy as np
 import face_recognition
 
-imgShekhar = face_recognition.load_image_file("D:\Python Projects\Face Recognition and Attendance\Images\Shekhar.jpg")
-imgShekhar = cv2.cvtColor(imgShekhar,cv2.COLOR_BGR2RGB)
+imgPhoto = face_recognition.load_image_file("Path of sample photograph")
+imgPhoto = cv2.cvtColor(imgPhoto,cv2.COLOR_BGR2RGB)
 
-imgTest = face_recognition.load_image_file("D:\Python Projects\Face Recognition and Attendance\Images\TestShekhar.jpg")
+imgTest = face_recognition.load_image_file("Path of the test photograph")
 imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
 
-faceLoc = face_recognition.face_locations(imgShekhar)[0]
-encodeShekhar = face_recognition.face_encodings(imgShekhar)[0]
-cv2.rectangle(imgShekhar,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(6, 112, 8, 1),2)
+faceLoc = face_recognition.face_locations(imgPhoto)[0]
+EncodeImg = face_recognition.face_encodings(imgPhoto)[0]
+cv2.rectangle(imgPhoto,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(6, 112, 8, 1),2)
 
 faceLocTest = face_recognition.face_locations(imgTest)[0]
 encodeTest = face_recognition.face_encodings(imgTest)[0]
 cv2.rectangle(imgTest,(faceLocTest[3],faceLocTest[0]),(faceLocTest[1],faceLocTest[2]),(6, 112, 8, 1),2)
 
-results = face_recognition.compare_faces([encodeShekhar],encodeTest)
+results = face_recognition.compare_faces([EncodeImg],encodeTest)
 print(results)
 
-faceDis = face_recognition.face_distance([encodeShekhar],encodeTest)
+faceDis = face_recognition.face_distance([EncodeImg],encodeTest)
 print(faceDis) 
 
 cv2.putText(imgTest,f'{results}{round(faceDis[0],2)}',(40,40),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
 
-cv2.imshow("Shekhar Yadav",imgShekhar)
-cv2.imshow("Shekhar Yadav Test",imgTest)
+cv2.imshow("Img",imgPhoto)
+cv2.imshow("Test",imgTest)
 cv2.waitKey(0)
 
 
