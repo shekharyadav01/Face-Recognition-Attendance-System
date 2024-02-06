56f459b5137351161bbd8612d992132b147000a8
Shekhar Yadav <114596028+shekharyadav01@users.noreply.github.com>
2024-02-06 20:04:58 +0530

    Add files via upload

---
+++ AttendanceProject.py
@@ -0,0 +1,72 @@
+import cv2
+import face_recognition
+import numpy as np
+import os
+from datetime import datetime
+
+path = "D:\Python Projects\Face Recognition and Attendance\Images"
+images = []
+className = []
+mylist = os.listdir(path)
+print(mylist)
+
+for cls in mylist:
+    curImg = cv2.imread(f'{path}/{cls}')
+    images.append(curImg)
+    className.append(os.path.splitext(cls)[0])
+print(className)
+
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+def markAttendance(name):
+    with open("attendance.csv",'r+')as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
+             
+    
+
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    seccess,img=cap.read()    
+    facesCurFrame = face_recognition.face_locations(img)
+    encodeCurFrame = face_recognition.face_encodings(img,facesCurFrame) 
+
+    for encodeFace,faceLoc in zip(encodeCurFrame,facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
+        #print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = className[matchIndex].upper()
+            print(name)
+
+            y1,x2,y2,x1 = faceLoc
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-40),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
+            markAttendance(name)
+
+    cv2.imshow('Webcam',img)
+    cv2.waitKey(1)
+
+    
---
+++ main.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+import face_recognition
+
+imgShekhar = face_recognition.load_image_file("D:\Python Projects\Face Recognition and Attendance\Images\Shekhar.jpg")
+imgShekhar = cv2.cvtColor(imgShekhar,cv2.COLOR_BGR2RGB)
+
+imgTest = face_recognition.load_image_file("D:\Python Projects\Face Recognition and Attendance\Images\TestShekhar.jpg")
+imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
+
+faceLoc = face_recognition.face_locations(imgShekhar)[0]
+encodeShekhar = face_recognition.face_encodings(imgShekhar)[0]
+cv2.rectangle(imgShekhar,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(6, 112, 8, 1),2)
+
+faceLocTest = face_recognition.face_locations(imgTest)[0]
+encodeTest = face_recognition.face_encodings(imgTest)[0]
+cv2.rectangle(imgTest,(faceLocTest[3],faceLocTest[0]),(faceLocTest[1],faceLocTest[2]),(6, 112, 8, 1),2)
+
+results = face_recognition.compare_faces([encodeShekhar],encodeTest)
+print(results)
+
+faceDis = face_recognition.face_distance([encodeShekhar],encodeTest)
+print(faceDis) 
+
+cv2.putText(imgTest,f'{results}{round(faceDis[0],2)}',(40,40),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+
+cv2.imshow("Shekhar Yadav",imgShekhar)
+cv2.imshow("Shekhar Yadav Test",imgTest)
+cv2.waitKey(0)
+
+
